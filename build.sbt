63ee0e6d1e076d2a362f74a8e411201280ce0367
James B Greer <jbgreer@gmail.com>
2020-12-26 12:11:44 -0600

    initial

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "ScalaGraphExample"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+libraryDependencies += "com.assembla.scala-incubator" %% "graph-core" % "1.11.0"
+    
---
+++ src/main/scala/ScalaGraphExample.scala
@@ -0,0 +1,11 @@
+import scalax.collection.Graph
+import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
+
+
+/**
+  * Created by jbgreer on 7/24/16.
+  */
+object ScalaGraphExample extends App {
+
+
+}
